62f8c7cea9023a0be77f62afa05e06f7b4f5aa06
cucibaju123 <cucibaju123@gmail.com>
2022-04-05 15:05:36 +0700

    Initial commit
    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'break-750': '750px',
+      },
+      letterSpacing: {
+        '6': '.6em',
+      },
+      colors: {
+        'neutral': '#F8FAF8',
+        'primary': '#CDDEEC',
+        'dark': '#5C707F',
+        'accent': '#EF798A',
+        'accent_bright': '#7FD1AE',
+        'cream': 'EFD8C1'
+      },
+      fontFamily: {
+        lora: ["Lora", "serif"],
+        alice: ["Alice", "serif"],
+        lemon: ["Lemon", "sans-serif"],
+
+      }
+    },
+  },
+  plugins: [],
+}
