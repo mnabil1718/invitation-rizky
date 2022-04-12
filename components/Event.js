57f5b7579984ddeac69b898a363daed908b73561
cucibaju123 <cucibaju123@gmail.com>
2022-04-12 13:02:51 +0700

    Testing maps embed API

--- components/Event.js
+++ components/Event.js
@@ -30,6 +30,9 @@ export default function Event() {
                 </div>
 
             </div>
+
+            <iframe width="600" height="450" style="border:0" loading="lazy" allowFullScreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJu29NqajPaS4RnkNYU2L1u0Q&key=AIzaSyB737Dw7OHUuvAbAtKloX8L-8TFVep3ZwI"></iframe>
+
         </section>
     )
 }
