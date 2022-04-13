a780b8a0a7309b0c87f0cb8dea0774ac6558a129
cucibaju123 <cucibaju123@gmail.com>
2022-04-13 18:49:27 +0700

    test animation changed to fade

--- components/Showcase.js
+++ components/Showcase.js
@@ -15,15 +15,15 @@ export default function Showcase() {
                 <div className="absolute -top-6 left-0">
                     <Image src="/svg/birdtrail.svg" alt="Dashed line" width={169} height={117} />
                 </div>
-                <motion.div variants={showcaseVariants.ornamentRight} initial="initial" animate="animate" className="absolute right-9">
+                <motion.div variants={showcaseVariants.fade} initial="initial" animate="animate" className="absolute right-9">
                     <Image className='drop-shadow-md' src="/png/cloud3.png" alt="cloud" width={97} height={97} />
                 </motion.div>
             </div>
             <div className="max-w-screen-sm relative text-center mx-auto">
-                <motion.div variants={showcaseVariants.ornamentLeft} initial="initial" animate="animate" className="absolute z-10 left-0 top-14">
+                <motion.div variants={showcaseVariants.fade} initial="initial" animate="animate" className="absolute z-10 left-0 top-14">
                     <Image className='drop-shadow-md' src="/png/cloud.png" alt="cloud" width={98} height={50} />
                 </motion.div>
-                <motion.div variants={showcaseVariants.ornamentRight} initial="initial" animate="animate" className="absolute z-10 -right-14 top-16">
+                <motion.div variants={showcaseVariants.fade} initial="initial" animate="animate" className="absolute z-10 -right-14 top-16">
                     <Image className='drop-shadow-md' src="/png/cloud2.png" alt="cloud" width={143} height={116} />
                 </motion.div>
                 <motion.div variants={showcaseVariants.invitedVariants} initial="initial" animate="animate">
@@ -48,7 +48,7 @@ export default function Showcase() {
                 </motion.div>
             </div>
             <div className="max-w-screen-sm mx-auto overflow-y-clip relative h-28">
-                <motion.div variants={showcaseVariants.ornamentLeft} initial="initial" animate="animate" className="absolute bottom-8 -left-10">
+                <motion.div variants={showcaseVariants.fade} initial="initial" animate="animate" className="absolute bottom-8 -left-10">
                     <Image className='drop-shadow-md' src="/png/cloud3.png" alt="cloud" width={97} height={97} />
                 </motion.div>
                 <motion.div variants={showcaseVariants.fade} initial="initial" animate="animate" className="absolute z-10 bottom-12 right-28">
