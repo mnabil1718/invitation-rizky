1ccf4fdd814305a1f55b6e7c2e63b43c5cf9f0e3
cucibaju123 <cucibaju123@gmail.com>
2022-04-14 12:25:10 +0700

    Play audio enabled

--- components/SoundButton.js
+++ components/SoundButton.js
@@ -15,7 +15,7 @@ export default function SoundButton({ playPauseHandler, isPlaying }) {
 
     return (
         <motion.div variants={soundVariants.variants} initial="initial" animate="animate" className="fixed z-30 top-5 right-5">
-            <ReactHowler playing={false} src={BgSound} loop />
+            <ReactHowler playing={isPlaying} src={BgSound} loop />
             <button className="w-16 h-16 rounded-full bg-white/70 backdrop-blur-md drop-shadow-xl" onClick={() => playPauseHandler()}>{isPlaying ? (<FontAwesomeIcon icon={faPause} size='2x' />) : (<FontAwesomeIcon icon={faPlay} size='2x' />)}</button>
         </motion.div>
     )
