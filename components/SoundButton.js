a0dcfc041311697fcbac50604d3a5ffeedb6f81e
cucibaju123 <cucibaju123@gmail.com>
2022-04-14 22:56:42 +0700

    get larger image

--- components/SoundButton.js
+++ components/SoundButton.js
@@ -14,7 +14,7 @@ export default function SoundButton({ playPauseHandler, isPlaying }) {
 
     return (
         <motion.div initial={{ x: 100, rotate: 180 }} animate={{ x: 0, rotate: 0 }} transition={{ duration: 0.6, type: 'spring', delay: 0.5 }} className="fixed z-30 top-5 right-5">
-            <ReactHowler playing={isPlaying} src={BgSound} loop />
+            <ReactHowler playing={false} src={BgSound} loop />
             <button className="w-16 h-16 rounded-full bg-white/70 backdrop-blur-md drop-shadow-xl" onClick={() => playPauseHandler()}>{isPlaying ? (<FontAwesomeIcon icon={faPause} size='2x' />) : (<FontAwesomeIcon icon={faPlay} size='2x' />)}</button>
         </motion.div>
     )
